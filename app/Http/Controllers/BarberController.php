668bf17647e756f8ac893fcc6a17ac0192f54649
sha-0x41414141 <87914329+sha-0x41414141@users.noreply.github.com>
2025-02-28 08:51:19 +0100

    barber_delete

--- app/Http/Controllers/BarberController.php
+++ app/Http/Controllers/BarberController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Exception;
 use Illuminate\Http\Request;
 use App\Models\Barber;
 use Illuminate\Validation\ValidationException;
@@ -37,8 +38,34 @@ class BarberController extends Controller
         return response()->json(['success' => true, 'message' => $barber->barber_name . " barber sikeresen rögzítve"], 200, options: JSON_UNESCAPED_UNICODE);
     }
 
-    public function destroy()
+    public function destroy(Request $request)
     {
+        try{
+            $request->validate(
+                [
+                    'id' => 'required|integer|exists:barbers,id'
+                ],
+                [
+                    'required' => 'A(z) :attribute mező megadása kötelező!',
+                    'integer' => 'A(z) :attribute mezőnek szöveg típusúnak kell lennie!',
+                    'exists' => 'Nem létezik ilyen nevű barber'
+                ]
+            );
+        }
+        catch(ValidationException $e)
+        {
+            return response()->json(['success' => false, 'message' => $e->errors()], 400, options: JSON_UNESCAPED_UNICODE);
+        }
 
+        try{
+            $barber = Barber::findOrFail($request->id);
+            $barber->delete();
+        }
+        catch(Exception $e)
+        {
+            return response()->json(['success' => false, 'message' => "Sikertelen törlés!"], 400, options: JSON_UNESCAPED_UNICODE);
+        }
+
+        return response()->json(['success' => true, 'message' => "Sikeres törlés"], 200, options: JSON_UNESCAPED_UNICODE);
     }
 }
