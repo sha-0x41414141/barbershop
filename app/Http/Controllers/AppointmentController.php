c03fbaf7af1f811399ed945bec077302a312df73
sha-0x41414141 <87914329+sha-0x41414141@users.noreply.github.com>
2025-02-28 09:07:42 +0100

    appointments

--- app/Http/Controllers/AppointmentController.php
+++ app/Http/Controllers/AppointmentController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Appointment;
+use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Validation\ValidationException;
 
@@ -10,12 +11,12 @@ class AppointmentController extends Controller
 {
     public function index()
     {
-        $appointment = Appointment::load("barber");
+        $appointment = Appointment::with('barber')->get();
         return response()->json($appointment, 200, options: JSON_UNESCAPED_UNICODE);
     }
     public function store(Request $request)
     {
-        try{
+        try {
             $request->validate(
                 [
                     'name' => 'required|string|max:255',
@@ -31,17 +32,39 @@ class AppointmentController extends Controller
                     'exists' => 'Nem létezik barber ezzel az id-val'
                 ]
             );
-        }
-        catch(ValidationException $e)
-        {
+        } catch (ValidationException $e) {
             return response()->json(['success' => false, 'message' => $e->errors()], 400, options: JSON_UNESCAPED_UNICODE);
         }
 
         $appointment = Appointment::create($request->all());
         return response()->json(['success' => true, 'message' => $appointment->name . " névvel időpont sikeresen rögzítve!"], 200, options: JSON_UNESCAPED_UNICODE);
     }
-    public function destroy()
+    public function destroy(Request $request)
     {
-        
+        try {
+            $request->validate(
+                [
+                    "id" => 'required|integer|exists:appointments,id'
+                ],
+                [
+                    'required' => 'A(z) :attribute mező megadása kötelező!',
+                    'integer' => 'A(z) :attribute mezőnek integer típusúnak kell lennie',
+                    'exists' => 'Nem létezik barber ezzel az id-val'
+                ]
+            );
+        } catch (ValidationException $e) {
+            return response()->json(['success' => false, 'message' => $e->errors()], 400, options: JSON_UNESCAPED_UNICODE);
+        }
+
+        try{
+            $appointment = Appointment::findOrFail($request->id);
+            $appointment->delete();
+        }
+        catch(Exception $e)
+        {
+            return response()->json(['success' => false, 'message' => $e], 400, options: JSON_UNESCAPED_UNICODE);
+        }
+
+        return response()->json(['success' => true, 'message' => "Időpont sikeresen törölve!"], 200, options: JSON_UNESCAPED_UNICODE);
     }
 }
