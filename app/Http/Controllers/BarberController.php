bd5a163616e8c32e0a0eb932959a5a5255ffa9e6
sha-0x41414141 <87914329+sha-0x41414141@users.noreply.github.com>
2025-02-28 08:45:47 +0100

    barber_store

--- app/Http/Controllers/BarberController.php
+++ app/Http/Controllers/BarberController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Barber;
+use Illuminate\Validation\ValidationException;
 
 class BarberController extends Controller
 {
@@ -14,19 +15,25 @@ class BarberController extends Controller
 
     public function store(Request $request)
     {
-        $request->validate(
-            [
-                'barber_name' => 'required|string|max:255'
-            ],
-            [
-                'required' => 'A(z) :attribute mező megadása kötelező!',
-                'string' => 'A(z) :attribute mezőnek szöveg típusúnak kell lennie!',
-                'max' => 'A(z) :attribute mező túl hosszú. Max :max'
-            ]
-        );
-
+        try{
+            $request->validate(
+                [
+                    'barber_name' => 'required|string|max:255'
+                ],
+                [
+                    'required' => 'A(z) :attribute mező megadása kötelező!',
+                    'string' => 'A(z) :attribute mezőnek szöveg típusúnak kell lennie!',
+                    'max' => 'A(z) :attribute mező túl hosszú. Max :max'
+                ]
+            );
+        }
+        catch(ValidationException $e)
+        {
+            return response()->json(['success' => false, 'message' => $e->errors()], 400, options: JSON_UNESCAPED_UNICODE);
+        }
+        
         $barber = Barber::create($request->all());
-        return response()->json(['success' => true, 'message' => $barber->barber_name . " barber sikeresen rögzítve"], 200);
+        return response()->json(['success' => true, 'message' => $barber->barber_name . " barber sikeresen rögzítve"], 200, options: JSON_UNESCAPED_UNICODE);
     }
 
     public function destroy()
